173fff6f9453a416342a9b6c311e70f4bf0150ca
compliment <elbasuv@gmail.com>
2018-12-10 19:01:08 +0530

    changed to 1hosts

--- scripts/gen-adblock-refuse.sh
+++ scripts/gen-adblock-refuse.sh
@@ -1,1 +1,1 @@
-(curl --silent https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | grep '^0\.0\.0\.0' | sort) | awk '{print "local-zone: \""$2"\" refuse"}'
+(curl --silent https://1hos.cf | grep '^0\.0\.0\.0' | sort) | awk '{print "local-zone: \""$2"\" refuse"}'
